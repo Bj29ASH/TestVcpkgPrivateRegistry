d76a81cfca7f9728bf43425a187f9d4c8fcc4ceb
akash <akash@launchtrax.com>
2024-09-10 18:28:55 +0530

    Tested simdis simcore;

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 #include <DemoAdd/demo_add.h>
 #include <QApplication>
 #include <QDebug>
+#include <simCore/Calc/Angle.h>
 
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
 
   auto res = demo::add(10, 20);
   qDebug() << "result is " << res;
+
+  simCore::Vec3 tmp{};
 
   return app.exec();
 }
